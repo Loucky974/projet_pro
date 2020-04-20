ac900a7e97f505acab87b3d010f52d0e6f378902
Loucky974 <clucie97495@gmail.com>
2020-04-20 02:42:32 -0300

    p_v1

---
+++ resources/views/download.blade.php
@@ -0,0 +1,157 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+   <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>IRD IMAGES</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: white;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 90vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 80vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            
+        </style>
+    </head>
+    <body>
+
+        <header>
+        
+            <a href="#"><img src='ird.png' alt="logo_IRD" id="logoird" /></a>
+        </header>
+        <div class="flex-center position-ref full-height">
+
+      
+
+        
+       
+            <form action=/post> 
+  <label for="DateD">Date Début:</label>
+  <input type="date" id="DateD" name="DateD">
+  
+
+  <label for="DateF">Date Fin:</label>
+  <input type="date" id="DateF" name="DateF">
+  
+
+  <label for="Mode">Mode:</label>
+
+  <input type="number" min="1" max="6"  name="Mode">
+
+  <label for="Canal">Canal: </label>
+<input type="number" min="1" max="2"  name="Canal">
+<br><br><br>
+<p><input  id="sub" type="submit" value="Télécharger"> </p>
+
+</form>
+<style>
+#logoird {
+    display: inline-block;
+    margin-bottom: 0px;
+    margin-top: 20px;
+    margin-left: 20px;
+}
+
+input {
+    border: 1px solid gray;
+    border-radius: 4px;
+    font-size: 22px;
+
+  }
+label{
+    font-size: 22px;
+    color:white;
+}
+
+body { background-image: url('deux.jpg');
+  background-repeat: no-repeat;
+  background-size:cover;
+  max-width: 100%;
+  height: auto;
+ 
+
+}
+
+#sub {
+    display:block;
+    margin-right: auto;
+    margin-left: auto;
+    
+}
+</style>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
+
+        
+                
+               
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                   
+               
+         
+    </div>
+</div>
+@endsection
---
+++ resources/views/productAjax.blade.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>IRD</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+    <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+</head>
+<body>
+    
+<div class="container">
+    <h1>Bienvenue sur l'espace Administrateur</h1>
+    <a class="btn btn-success" href="javascript:void(0)" id="createNewProduct"> Create New Product</a>
+    <table class="table table-bordered data-table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Mail</th>
+                <th>Role</th>
+                <th width="200px">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</div>
+   
+<div class="modal fade" id="ajaxModel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="modelHeading"></h4>
+            </div>
+            <div class="modal-body">
+                <form id="productForm" name="productForm" class="form-horizontal">
+                   <input type="hidden" name="product_id" id="product_id">
+                    <div class="form-group">
+                        <label for="name" class="col-sm-2 control-label">Nom</label>
+                        <div class="col-sm-12">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Nom" value="" maxlength="50" required="">
+                        </div>
+                    </div>
+     
+                    <div class="form-group">
+                        <label for="email" class="col-sm-2 control-label">Email</label>
+                        <div class="col-sm-12">
+                            <input type="text" class="form-control" id="email" name="email" placeholder="Enter email" value="" maxlength="50" required="">
+                        </div>
+                    </div>
+
+                 
+                    
+
+                    <div class="form-group">
+                        <label for="company-content"class="col-sm-2 control-label">Role</label>
+                        <select name="role" id="role" class="form-control">
+                        <option>admin</option>
+                        <option>user</option>
+                        <option>visiteur</option>
+                        </select>
+                        </div>
+
+
+                    <div class="col-sm-offset-2 col-sm-10">
+                     <button type="submit" class="btn btn-primary" id="saveBtn" value="create">Save changes
+                     </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+    
+</body>
+    
+<div class="col-sm-offset-2 col-sm-10">
+    <button type="submit" class="btn btn-succes" id="retour" value="" onclick="location.href='{{url('/admin')}}'">Retour 
+    </button>
+   </div>
+
+   <style>
+       #retour{
+           margin: 7%;
+       }
+   </style>
+<script type="text/javascript">
+  $(function () {
+     
+      $.ajaxSetup({
+          headers: {
+              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+          }
+    });
+    
+    var table = $('.data-table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: "{{ route('ajaxproducts.index') }}",
+        columns: [
+           // {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+            {data: 'name', name: 'name'},
+            {data: 'email', name: 'email'},
+            {data: 'role', name: 'role'},
+            {data: 'action', name: 'action', orderable: false, searchable: false},
+        ]
+    });
+     
+    $('#createNewProduct').click(function () {
+        $('#saveBtn').val("create-product");
+        $('#product_id').val('');
+        $('#productForm').trigger("reset");
+        $('#modelHeading').html("Create New Product");
+        $('#ajaxModel').modal('show');
+    });
+    
+    $('body').on('click', '.editProduct', function () {
+      var product_id = $(this).data('id');
+      $.get("{{ route('ajaxproducts.index') }}" +'/' + product_id +'/edit', function (data) {
+          $('#modelHeading').html("Edit Product");
+          $('#saveBtn').val("edit-user");
+          $('#ajaxModel').modal('show');
+          $('#product_id').val(data.id);
+          $('#name').val(data.name);
+          $('#email').val(data.email);
+          $('#role').val(data.role);
+         
+          
+      })
+   });
+    
+    $('#saveBtn').click(function (e) {
+        e.preventDefault();
+        $(this).html('Sending..');
+    
+        $.ajax({
+          data: $('#productForm').serialize(),
+          url: "{{ route('ajaxproducts.store') }}",
+          type: "POST",
+          dataType: 'json',
+          success: function (data) {
+     
+              $('#productForm').trigger("reset");
+              $('#ajaxModel').modal('hide');
+              table.draw();
+         
+          },
+          error: function (data) {
+              console.log('Error:', data);
+              $('#saveBtn').html('Save Changes');
+          }
+      });
+    });
+    
+    $('body').on('click', '.deleteProduct', function () {
+     
+        var product_id = $(this).data("id");
+        confirm("Are You sure want to delete !");
+      
+        $.ajax({
+            type: "DELETE",
+            url: "{{ route('ajaxproducts.store') }}"+'/'+product_id,
+            success: function (data) {
+                table.draw();
+            },
+            error: function (data) {
+                console.log('Error:', data);
+            }
+        });
+    });
+     
+  });
+</script>
+</html>
