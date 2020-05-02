7e2101e6ea3fcf9fd649483a3627147e55561d81
Loucky974 <clucie97495@gmail.com>
2020-05-01 23:40:20 -0300

    test messagerie

--- database/migrations/2018_03_06_151601_create_contacts_table.php
+++ database/migrations/2018_03_06_151601_create_contacts_table.php
@@ -32,9 +32,8 @@ class CreateContactsTable extends Migration
      */
     public function down()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+      
         Schema::dropIfExists('contacts');
-        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
      
     }
 }
