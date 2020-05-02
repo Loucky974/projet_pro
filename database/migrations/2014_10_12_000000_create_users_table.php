d9b4e381d7d6d987e24356ce30c78d8d571437f9
Loucky974 <clucie97495@gmail.com>
2020-05-02 00:05:54 -0300

    test messagerie

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->dropForeign('answers_user_id_foreign');
             
         });
     }
