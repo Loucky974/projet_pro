fecb69f9bec07093ce1c6aa400ff98188c41a531
Loucky974 <clucie97495@gmail.com>
2020-05-07 08:04:29 -0300

    V13

--- resources/views/image-gallery.blade.php
+++ resources/views/image-gallery.blade.php
@@ -265,7 +265,6 @@
     <div class="row">
     <div class='list-group gallery'>
 
-
             @if($images->count())
                 @foreach($images as $image)
                 <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -276,7 +276,6 @@ padding: 10px 80px;
     <div class="row">
     <div class='list-group gallery'>
 
-
             @if($images->count())
                 @foreach($images as $image)
                 <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
